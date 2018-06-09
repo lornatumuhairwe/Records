aaaa4432e24ba9da4fba3b342fa3cd7a849fb0b9
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2018-06-09 17:10:39 +0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'static_page/index'
+  root to: 'static_page#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
